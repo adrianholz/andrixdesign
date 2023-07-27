3b92924178dd55051b70cae2590bede25cf14d02
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2023-07-27 16:10:25 -0300

    Final Changes

--- js/script-global.js
+++ js/script-global.js
@@ -129,9 +129,21 @@ function faq() {
   });
 }
 
+function anchor() {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
 toggleMobileMenu();
 mobileSubmenu();
 alterFormCheckboxes();
 animateElements();
 tooltip();
 faq();
+anchor();
